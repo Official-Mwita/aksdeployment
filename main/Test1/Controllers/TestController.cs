1ff03e05e2878806711a0695ef90b9080dca7ff5
MwitaJ <chachajoseph109@gmail.com>
2023-08-23 16:19:26 +0300

    first commit

---
+++ main/Test1/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+        [Route("/")]
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return Ok("This is the home for test 1");
+        }
+    }
+}
---
+++ main/Test2/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [Route("/")]
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return Ok("This is the home for test 2");
+        }
+    }
+}
